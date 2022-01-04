df62d1700e1802416c6e01ba6d854ab40142afc2
hidan12 <marcarfe12@gmail.com>
2022-01-04 14:36:11 -0500

    add file 1

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    tam = len(my_list)
+    if (idx >= tam or idx < 0):
+        return(None)
+    else:
+        my_list[idx] = element
+        return(my_list)
