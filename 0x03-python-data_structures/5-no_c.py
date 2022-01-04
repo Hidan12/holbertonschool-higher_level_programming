96c1fb0919536d964031c7cb29a443a7d0214359
hidan12 <marcarfe12@gmail.com>
2022-01-04 15:44:41 -0500

    add file 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new = []
-    if my_string is not None:
-        for a in my_string:
-            if (a != 'c' and a != 'C'):
-                new.append(a)
-        return(new)
+    for a in my_string:
+        if (a != 'c' and a != 'C'):
+            new.append(a)
+    return(new)
