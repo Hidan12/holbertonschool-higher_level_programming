f375d11b2baa2111ea8475c912c9f0d79c35a0d0
hidan12 <marcarfe12@gmail.com>
2022-01-04 23:22:52 -0500

    add file 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    div = []
+    for a in range(len(my_list)):
+        if (my_list[a] % 2):
+            div.append(False)
+        else:
+            div.append(True)
+    return (div)
+    
