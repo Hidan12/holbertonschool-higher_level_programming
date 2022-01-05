30d19d680a238eca0d9a9bb69b3f3dfbfa33711b
hidan12 <marcarfe12@gmail.com>
2022-01-05 11:04:03 -0500

    add file 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = []
+    for a in range (len(my_list)):
+        if (my_list[a] != search):
+            new.append(my_list[a])
+        else:
+            new.append(replace)
+    return(new)
+
