93ad19ddd1c03c2cfbf1333805a887e8776181f1
hidan12 <marcarfe12@gmail.com>
2022-01-04 22:55:19 -0500

    add file 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     tam = len(sentence)
-    return(tam, sentence[0])
+    if sentence == "":
+        return(tam, None)
+    else:
+        return(tam, sentence[0])
