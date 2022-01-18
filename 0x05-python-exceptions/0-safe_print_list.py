af8ab0a63a4c54ff14cfb37c6326874275797d65
hidan12 <marcarfe12@gmail.com>
2022-01-18 15:49:37 -0500

    add file 0 and readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    try:
+        for a in range(x):
+            print("{:d}".format(my_list[a]), end="")
+            a += 1
+    except:
+        pass
+    print("")
+    return(a)
