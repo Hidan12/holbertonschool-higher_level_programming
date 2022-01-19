c4bcf429e0ece8e2d84042b8e115a246e0ade3e8
hidan12 <marcarfe12@gmail.com>
2022-01-18 22:00:46 -0500

    add file 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,7 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
             except (ValueError, TypeError):
                 print("wrong type")
                 result.append(0)
-    except:
+    except IndexError:
         print("out of range")
         result.append(0)
     return(result)
