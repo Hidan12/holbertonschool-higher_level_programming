6a1079f4a04155e49678c13a43dac10afe60cb87
hidan12 <marcarfe12@gmail.com>
2022-01-19 23:27:55 -0500

    add file 1 and file 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script that assigns a private instance attribute size to class
-    Square with size validation"""
+    Square"""
 class Square:
     def __init__(self, size = 0):
         if (size >= 0):
