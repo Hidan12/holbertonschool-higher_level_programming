85a42e31b6d0a0edeb396c5f02d3f1928fec6561
hidan12 <marcarfe12@gmail.com>
2022-01-20 03:38:33 -0500

    add file 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Script that assigns a private instance attribute size to class
+    Square with size validation"""
+
+
+class Square:
+    """Class that defines a square with a private instance attribute size"""
+
+    def __init__(self, size=0):
+        if (size >= 0):
+            self.__size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value=0):
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return(int(self.__size) * int(self.__size))
+
+    def my_print(self):
+        a = int(self.__size)
+        b = 0
+        c = 0
+        for b in range(a):
+            for c in range(a):
+                print("#", end="")
+            print("")
