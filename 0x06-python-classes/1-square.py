ed0188c7578b88d2c09381e3f52b2da94250da7b
hidan12 <marcarfe12@gmail.com>
2022-01-19 23:34:47 -0500

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Script that assigns a private instance attribute size to class
     Square"""
+
+
 class Square:
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if (size >= 0):
             self._size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Script that assigns a private instance attribute size to class
     Square with size validation"""
+
+
 class Square:
     def __init__(self, size=0):
         if type(size) is int:
