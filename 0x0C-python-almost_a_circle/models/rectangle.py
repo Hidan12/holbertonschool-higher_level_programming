0d0ac659a87cd9b931930f8d397b2f595c86137c
hidan12 <marcarfe12@gmail.com>
2022-02-09 23:16:54 -0500

    add files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 from models.base import Base
 
+
 class Rectangle(Base):
+
     """Rectangle class"""
+
     __width = 0
     __height = 0
     __x = 0
     __y = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
+
         """Iniciatation method"""
-        self.width = width    
+
+        self.width = width
         self.height = height
         self.__x = x
         self.__y = y
@@ -19,21 +24,23 @@ class Rectangle(Base):
     @property
     def get_width(self):
         return(self.__width)
-    
-    @width.setter    
+
+    @width.setter
     def width(self, width):
+
         """width setter"""
+
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
-    
-   
-    
+
     @height.setter
     def height(self, height):
+
         """height setter"""
+
         if type(height) != int:
             raise TypeError("height must be an integer")
         if height <= 0:
@@ -43,10 +50,12 @@ class Rectangle(Base):
     @property
     def get_height(self):
         return(self.__height)
-    
+
     @set_x.setter
     def set_x(self, x):
+
         """x setter"""
+
         if type(x) != int:
             raise TypeError("x must be an integer")
         if x < 0:
@@ -59,13 +68,15 @@ class Rectangle(Base):
 
     @set_y.setter
     def set_y(self, y):
+
         """y setter"""
+
         if type(y) != int:
             raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
-    
+
     @property
     def get_y(self):
         return(self.__y)
